f2d252e3aa14437850a5547933c1bfd879f4e6ef
Anudeep-Nayakoti <nayakotianudeep@yahoo.com>
2019-11-03 11:14:08 -0700

    added method for grabing the userInputs of the department

--- SimpleEmailApp/src/Emailapp/EmailApp.java
+++ SimpleEmailApp/src/Emailapp/EmailApp.java
@@ -4,6 +4,8 @@ public class EmailApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		EmailBluePrint em1 = new EmailBluePrint("Anudeep", "Nayakoti");
 
 	}
 
--- SimpleEmailApp/src/Emailapp/EmailBluePrint.java
+++ SimpleEmailApp/src/Emailapp/EmailBluePrint.java
@@ -1,5 +1,7 @@
 package Emailapp;
 
+import java.util.Scanner;
+
 public class EmailBluePrint {
 private String firstName;
 private String lastName;
@@ -11,8 +13,33 @@ private int capaciyOfEmail;
 
 //Need a constructor to receive both FirstName and LastName
 
+public EmailBluePrint(String fname, String lname) {
+this.firstName=fname;
+this.lastName=lname;
+
+System.out.println("Constructed Email for: "+this.firstName+ "  " +this.lastName);
+
+//As soon as the object is created, it prompts the user for the Department Name and stores.
+
+this.departmentName=setDepartmentName();
+System.out.println("Your Department is: "+this.departmentName);
+	
+}
 
 //Ask for the departmentName
+public String setDepartmentName() {
+ 
+ System.out.println("Here are the Codes for the Department:\n12:IT\n13:DESIGN\n17:APPLICATION DEVELOPMENT\n Enter your Department code:");
+ Scanner console=new Scanner(System.in);
+ int deptInput=console.nextInt();
+ if(deptInput==13) {return "Design";}
+ else if (deptInput==12){ return "IT";}
+ else if (deptInput==17) {return "Software developers";}
+ else {return "You don't belong here";}
+ 
+ 
+ 
+}
 
 
 //Create a random string password
@@ -29,5 +56,5 @@ private int capaciyOfEmail;
 
 
 
-
 }
+
