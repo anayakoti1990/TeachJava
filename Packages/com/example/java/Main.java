8e8809ce919d1ec1d97df6db829d8858e46b6e4d
Anudeep-Nayakoti <nayakotianudeep@yahoo.com>
2019-11-10 12:35:59 -0700

    Made a package using Notepad style

---
+++ Packages/com/example/java/Main.java
@@ -0,0 +1,9 @@
+package com.example.java;
+public class Main
+{
+   public static void main(String whatever[])
+   {
+     System.out.println("Hello! from Java");
+     System.out.println(whatever[0]);
+    }
+}
