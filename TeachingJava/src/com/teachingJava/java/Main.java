df827a421026dabe1b7cb8ef6dda1812e64d333c
Anudeep-Nayakoti <nayakotianudeep@yahoo.com>
2019-11-10 14:03:52 -0700

    intellij ide

---
+++ TeachingJava/src/com/teachingJava/java/Main.java
@@ -0,0 +1,12 @@
+package com.teachingJava.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello! from IntelliJ IDE");
+        String dummyargs="a";
+        //dummyargs="any";
+        System.out.println(dummyargs);
+    }
+}
