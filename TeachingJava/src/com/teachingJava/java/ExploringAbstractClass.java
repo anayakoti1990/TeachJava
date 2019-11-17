1ebc548d8ca8fcf6a2cf32dfafc5034a7a6f548e
Anudeep-Nayakoti <nayakotianudeep@yahoo.com>
2019-11-17 16:16:27 -0700

    Taught why abstract class cannot instantiate a class

---
+++ TeachingJava/src/com/teachingJava/java/ExploringAbstractClass.java
@@ -0,0 +1,16 @@
+package com.teachingJava.java;
+
+public abstract class ExploringAbstractClass {
+	
+	public static void main(String[] args) {
+		
+	//The rule of abstract class is that you cannot create an object. 
+		//The only way the abstract class is used by Inheritance concept.
+		//Abstract method does not have body as well. It can be used to run the inherited class method;
+		
+		//ExploringAbstractClass obj1=new ExploringAbstractClass();
+		System.out.println("Sorry we cannot instantiate an Object from an Abstract Class");
+		
+	}
+
+}
